ea7ed2aef5636deae4106c5c6fa1b59bc4858465
Chris Kamphuis <mail@chriskamphuis.com>
2016-12-22 23:46:28 +0100

    put gdc in function

--- numerical_algorithms/gdc.py
+++ numerical_algorithms/gdc.py
@@ -1,9 +1,6 @@
-import sys
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-while b != 0:
-    remainder = a % b
-    a = b
-    b = remainder
-print(a)
+def gdc(a, b):
+    while b != 0:
+        remainder = a % b
+        a = b
+        b = remainder
+    return a
