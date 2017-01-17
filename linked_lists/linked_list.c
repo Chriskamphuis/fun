bd1aa14ee17c51bab7ec7e42dda580e01b7bf5f9
Chris Kamphuis <mail@chriskamphuis.com>
2017-01-18 00:03:20 +0100

    Included a function for removing a list

--- linked_lists/linked_list.c
+++ linked_lists/linked_list.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-	  int x;
-		struct node *next;
+struct int_node{
+	int x;
+	struct int_node *next;
 };
 
-int main(){
-	struct node *root;	
-	root = malloc( sizeof(struct node) );  
+void free_linked_list(struct int_node *linked_list)
+{
+	while (linked_list != 0){
+		struct int_node *next = linked_list->next;
+		free(linked_list);
+		linked_list = next;
+	}
+}
+
+
+int main(void){
+	struct int_node *root;	
+	root = malloc(sizeof(struct int_node));  
 	root->next = 0;   
 	root->x = 12;
 	printf("%i\n", root->x);
