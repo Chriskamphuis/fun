3bf67e84cf55a5039688353ef5a1525d41697789
Chris Kamphuis <mail@chriskamphuis.com>
2017-01-18 22:10:31 +0100

    Include function for finding nodes

--- linked_lists/linked_list.c
+++ linked_lists/linked_list.c
@@ -3,10 +3,10 @@
 
 struct int_node{
 	int x;
-	struct int_node *next;
+	struct int_node* next;
 };
 
-void free_linked_list(struct int_node *linked_list)
+void free_list(struct int_node* linked_list)
 {
 	while(linked_list != 0){
 		struct int_node *next = linked_list->next;
@@ -26,7 +26,7 @@ void print_list(struct int_node* linked_list){
 	return;
 }
 
-void add_element(struct int_node *linked_list, int element){
+void add_element(struct int_node* linked_list, int element){
 	while(linked_list->next != 0){
 		struct int_node *next = linked_list->next;
 		linked_list = next;
@@ -37,15 +37,32 @@ void add_element(struct int_node *linked_list, int element){
 	return;
 }
 
+struct int_node* find_node_before(struct int_node* node, int target){
+  	if(node == NULL) return NULL;
+	while(node->next != NULL){
+		if(node->next->x == target) return node;
+		node = node->next;
+	}
+	return NULL;
+}
+
+struct int_node* find_node(struct int_node* node, int target){
+  	while(node != NULL){
+		if(node->x == target) return node;
+		node = node->next;
+	}
+  	return NULL;
+}
+
 int main(void){
-	struct int_node *root;	
+	struct int_node* root;	
 	root = malloc(sizeof(struct int_node));  
 	root->x = 12;
 	root->next = 0;
 	int i;
-	for(i=0; i<100; i++){
-		add_element(root, i%6);	
-	}
-	print_list(root);	
+	for(i=0; i<100; i++) add_element(root, i%20);	
+	struct int_node* a = find_node_before(root, 13);
+	print_list(root);
+	print_list(a);
 	return 0;
 }
